8285bc87ba54e0df4a80c14554c61297a40f514d
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-03-29 19:34:09 +0200

    added first projects

---
+++ uniqueValues.js
@@ -0,0 +1,18 @@
+const menu = [
+  { name: 'pancakes', category: 'breakfast' },
+  { name: 'burger', category: 'lunch' },
+  { name: 'steak', category: 'dinner' },
+  { name: 'bacon', category: 'breakfast' },
+  { name: 'eggs', category: 'breakfast' },
+  { name: 'pasta', category: 'dinner' },
+];
+
+const categories = ['all', ...new Set(menu.map((item) => item.category))];
+console.log(categories);
+
+const result = document.querySelector('.result');
+result.innerHTML = categories
+  .map((category) => {
+    return `<button>${category}</button>`;
+  })
+  .join('');
